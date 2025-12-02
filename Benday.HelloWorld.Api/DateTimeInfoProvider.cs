c4b3477e3b955567c1b3c0fb643f5a4ae306ec42
Ben Day <benday@benday.com>
2025-12-02 08:37:01 -0500

    Added new Api project and created basic IMessageProvider

---
+++ Benday.HelloWorld.Api/DateTimeInfoProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Benday.HelloWorld.Api;
+
+public class DateTimeInfoProvider : IMessageProvider
+{
+    public string GetMessage()
+    {
+        return $"The current date and time is: {DateTime.Now}";
+    }
+}
---
+++ Benday.HelloWorld.Api/IMessageProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Benday.HelloWorld.Api;
+
+public interface IMessageProvider
+{
+    string GetMessage();
+}
