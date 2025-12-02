e0846f680d30b7e391ff4e8d4c694c9e68315cd7
Ben Day <benday@benday.com>
2025-12-02 08:39:49 -0500

    Added message to hello world console app

--- Benday.HelloWorld/Program.cs
+++ Benday.HelloWorld/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Benday.HelloWorld.Api;
+
 Console.WriteLine("Hello, World!");
+
+IMessageProvider provider = new DateTimeInfoProvider();
+
+string message = provider.GetMessage();
+
+Console.WriteLine(message);
+
