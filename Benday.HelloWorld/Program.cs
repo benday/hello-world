1e37953c5c76f44f5a4ef275cf8320072e09770b
Ben Day <benday@benday.com>
2026-01-04 11:45:16 -0500

    Converted to not use top level statements.

--- Benday.HelloWorld/Program.cs
+++ Benday.HelloWorld/Program.cs
@@ -1,8 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Benday.HelloWorld.Api;
 
-Console.WriteLine("Hello, World!");
+namespace Benday.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
-IMessageProvider provider = new DateTimeInfoProvider();
+            IMessageProvider provider = new DateTimeInfoProvider();
 
-Console.WriteLine(provider.GetMessage());
+            Console.WriteLine(provider.GetMessage());
+        }
+    }
+}
